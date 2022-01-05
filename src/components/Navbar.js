1483ed772b078b430069d210c6e1d54278215a1f
Rafał Białowski <b.bialowski@gmail.com>
2022-01-05 09:42:59 +0100

    Add navigation

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,8 +1,75 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import "./navbar.scss";
+import logo from "../images/logo.svg";
+import menuOpen from "../images/icon-hamburger.svg";
+import menuClose from "../images/icon-close.svg";
+
+function Nav({ name }) {
+  return (
+    <ul className={name}>
+      <li>
+        <a href="#">About</a>
+      </li>
+      <li>
+        <a href="#">Careers</a>
+      </li>
+      <li>
+        <a href="#">Events</a>
+      </li>
+      <li>
+        <a href="#">Products</a>
+      </li>
+      <li>
+        <a href="#">Support</a>
+      </li>
+    </ul>
+  );
+}
 
 function Navbar() {
-  return <nav>Nav</nav>;
+  const [isOpen, setIsOpen] = useState(false);
+  const [changeColor, setChangeColor] = useState(false);
+  useEffect(() => {
+    let value = isOpen ? "translate(0)" : "translate(-110%)";
+    document.querySelector(".nav__mobile-menu").style.transform = value;
+  }, [isOpen]);
+  window.addEventListener("resize", () => {
+    if (window.innerWidth > 800) {
+      setIsOpen(false);
+    }
+  });
+  window.addEventListener("scroll", () => {
+    if (window.innerWidth > 800) return;
+    if (window.scrollY > 200) {
+      setChangeColor(true);
+    } else {
+      setChangeColor(false);
+    }
+  });
+  return (
+    <nav
+      className="nav"
+      style={
+        changeColor ? { backgroundColor: "#000" } : { backgroundColor: "#ccc" }
+      }
+    >
+      <div className="nav__logo-box">
+        <img src={logo} alt="logo" className="nav__logo" />
+      </div>
+      <Nav name="nav__desktop" />
+      <div className="nav__mobile">
+        <div className="nav__menu-box">
+          <img
+            src={isOpen ? menuClose : menuOpen}
+            alt="menu"
+            className="nav__menu"
+            onClick={() => setIsOpen((prevValue) => !prevValue)}
+          />
+        </div>
+        <Nav name="nav__mobile-menu" />
+      </div>
+    </nav>
+  );
 }
 
 export default Navbar;
--- src/containers/Header.js
+++ src/containers/Header.js
@@ -1,7 +1,13 @@
 import React from "react";
+import Navbar from "../components/Navbar";
 import "./header.scss";
 function Header() {
-  return <header>Header</header>;
+  return (
+    <header>
+      <Navbar />
+      Header
+    </header>
+  );
 }
 
 export default Header;
